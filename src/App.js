56be9eadc8ec78c56978deaa87df287106fcd2b8
fayez baig <coderninja16@gmail.com>
2020-08-05 14:44:16 +0530

    connecting alan to react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import alanbtn from "@alan-ai/alan-sdk-web";
+import "./App.css";
 
+const alankey =
+  "8f9b78127541ee0bf44c4792b9200f452e956eca572e1d8b807a3e2338fdd0dc/stage";
 function App() {
+  useEffect(() => {
+    alanbtn({
+      key: alankey,
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>AI NEWS APP</h1>
     </div>
   );
 }
